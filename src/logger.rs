12acb0c8d0d5cabde8b9f1c02e596b5a26bb3bd1
Devifish <devifish@outlook.com>
2020-05-09 20:52:15 +0800

    update: 默认日志Info等级

--- src/logger.rs
+++ src/logger.rs
@@ -2,10 +2,10 @@ use simplelog::*;
 
 /// init logger
 pub fn init() {
-    let term_logger = TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed);
+    let term_logger = TermLogger::new(LevelFilter::Info, Config::default(), TerminalMode::Mixed);
     let logger: Box<dyn SharedLogger> = match term_logger {
         Some(var) => var,
-        None => SimpleLogger::new(LevelFilter::Warn, Config::default()),
+        None => SimpleLogger::new(LevelFilter::Info, Config::default()),
     };
 
     CombinedLogger::init(vec![logger]).expect("No interactive terminal");
